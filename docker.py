c9811721b3345db105c9a755d00a8015cb8e38bb
Snow59 <82878319+Snow59@users.noreply.github.com>
2024-02-20 11:34:40 +0100

    Delete docker.py

--- docker.py
+++
@@ -1,9 +0,0 @@
-import docker
-
-client = docker.from_env()
-
-
-# Démarrer un conteneur MongoDB
-conteneur = client.containers.run("mongo", detach=True, ports={'27017/tcp': 27017}, name="mon-mongo")
-
-print(f"Conteneur démarré avec l'ID : {conteneur.id}")
