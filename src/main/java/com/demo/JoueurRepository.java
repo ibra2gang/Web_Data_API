574d212ce6dd43a0441c5b36a57698c7d8658aaa
boubeker ibrahim <ibrahimm2001@hotmail.fr>
2024-05-10 02:05:19 +0200

    modif

--- src/main/java/com/demo/JoueurRepository.java
+++ src/main/java/com/demo/JoueurRepository.java
@@ -2,5 +2,4 @@ package com.demo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JoueurRepository extends MongoRepository<Joueur, String> {
-    // Ajoutez les méthodes personnalisées si nécessaire
 }
